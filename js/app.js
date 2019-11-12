7b5387f6d37f8a710362cfdf295ebe91dc3c38e0
Kieran Barker <kieran11397@gmail.com>
2019-11-12 09:55:30 +0000

    Create a list of links for each h2 element

--- js/app.js
+++ js/app.js
@@ -1,5 +1,22 @@
 ;(function(d) {
 
+  var tableOfContents = d.querySelector("#table-of-contents");
+  var headings = Array.from(d.querySelectorAll("h2"));
 
+  function createLink(heading) {
+    return (
+      "<li>" +
+        "<a href='#" + heading.id + "'>" + heading.textContent + "</a>" +
+      "</li>"
+    );
+  }
+
+  tableOfContents.innerHTML = (
+    "<nav>" +
+      "<ol>" +
+        headings.map(createLink).join("") +
+      "</ol>" +
+    "</nav>"
+  );
 
 })(document);
