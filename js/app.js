5fd501ea6e16ec9c6de4c2db0e9d676f06e956fa
Kieran Barker <kieran11397@gmail.com>
2019-11-13 15:26:52 +0000

    Update script to add an ID to each heading that doesn't have one

--- js/app.js
+++ js/app.js
@@ -2,8 +2,21 @@
 
   "use strict";
 
+  /**
+   * Variables
+   */
+
   var tableOfContents = d.querySelector("#table-of-contents");
-  var headings = Array.from(d.querySelectorAll("h2[id]"));
+  var headings = Array.from(d.querySelectorAll("h2"));
+
+  /**
+   * Functions
+   */
+
+  function assignId(element) {
+    if (element.id) return;
+    element.id = element.textContent.replace(/\W+/g, "-").toLowerCase();
+  }
 
   function createLink(heading) {
     return (
@@ -13,8 +26,17 @@
     );
   }
 
+  /**
+   * Init
+   */
+
+  // Bail if there are no headings
   if (!headings.length) return;
 
+  // Assign each heading an ID if necessary
+  headings.forEach(assignId);
+
+  // Insert the table of contents
   tableOfContents.innerHTML = (
     "<nav>" +
       "<ol>" +
